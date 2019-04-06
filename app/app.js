594572abc8858ce5cad1aa5fda89f648b12914fc
maevetierney <maevet27@gmail.com>
2019-04-05 21:09:12 -0400

    Starting new project with angular 5

--- app/app.js
+++ app/app.js
@@ -1,14 +1,26 @@
 'use strict';
 
 // Declare app level module which depends on views, and core components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+// angular.module('myApp', [
+//   'ngRoute',
+//   'myApp.view1',
+//   'myApp.view2',
+//   'myApp.version'
+// ]).
+// config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+//   $locationProvider.hashPrefix('!');
+//
+//   $routeProvider.otherwise({redirectTo: '/view1'});
+// }]);
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+var app = angular.module('myApp', [
+    'myApp.version'
+]);
+
+app.controller('myController', ['$scope', '$http',function ($scope, $http) {
+  $http.get('messages.json').then(function(data, $scope, response){
+      $scope.messages = response.data;
+  }
+
+);
+ }]);
